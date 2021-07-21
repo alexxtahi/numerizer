54b3001759da65cc3a306f6f4c68b421d016c0b7
Konan Marylise <lysekonan3@gmail.com>
2021-07-21 10:28:06 +0000

    Adding packages

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:numerizer/main.dart';
+import 'package:numerizer/views/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
